f76fc5bc2bb7d7abc38e7013a192e7dc8b03057a
Jonatan Souza <jonatangd.souza@gmail.com>
2016-07-15 17:08:47 -0300

    fixed origin request with cors

--- config/express.js
+++ config/express.js
@@ -1,26 +1,32 @@
-var express = require('express');
-var load = require('express-load');
-var bodyParser = require('body-parser');
-var morgan = require('morgan');
+var express = require('express'),
+    load = require('express-load'),
+    bodyParser = require('body-parser'),
+    cors = require('cors'),
+    morgan = require('morgan');
 
 
-module.exports = function(){
+module.exports = function() {
     var app = express();
     app.use(morgan('dev'));
+    app.use(cors());
 
     app.set('port', 3000);
 
     app.use(express.static('./public'));
 
     app.set('view engine', 'ejs');
-    app.set('views','./app/views');
+    app.set('views', './app/views');
 
 
-    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
     app.use(bodyParser.json());
     app.use(require('method-override')());
 
-    load('models', {cwd: 'app'})
+    load('models', {
+            cwd: 'app'
+        })
         .then('controllers')
         .then('routes')
         .into(app);
