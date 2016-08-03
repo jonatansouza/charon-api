214d240c126a70f24fbe2e4934fa9e96ecc94b30
Jonatan Souza <jonatangd.souza@gmail.com>
2016-08-03 14:43:53 -0300

    init file included

--- config/openstack-config.js
+++ config/openstack-config.js
@@ -1,12 +1,14 @@
+var fs = require('fs');
+
+var config = JSON.parse(
+    fs.readFileSync(__dirname+'/../init.json')
+);
+
 exports.params = {
-    provider: 'openstack',
-    useServiceCatalog: true,
-    useInternal: false,
-    keystoneAuthVersion: 'v2',
-    username: 'admin',
-    password: 'password',
-    region: 'RegionOne',
-    apiKey: '8cf2c2c9100646d3b5ad1b77a2e60cce',
-    apiSecret: 'a59ac0ab7dd34a868b8881204ed4e8e6',
-    authUrl: 'http://146.134.235.249:5000'
+    provider: config.provider,
+    keystoneAuthVersion: config.keystoneAuthVersion,
+    username: config.username,
+    password: config.password,
+    region: config.region,
+    authUrl: config.authUrl
 }
